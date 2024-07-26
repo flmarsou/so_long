b47b0e0f0f1db1e61330c7dd106bc57ed96dc645
flmarsou <marsouinflorent12@gmail.com>
2024-07-26 13:01:49 +0200

    WIP Pathfinding (collectible not working)

--- src/map/path_map.c
+++ src/map/path_map.c
@@ -6,8 +6,96 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 11:37:33 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/07/25 15:47:48 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/07/26 12:59:39 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/so_long.h"
+
+// Entirely frees the floodfill map once the pathfinding is complete.
+static void	floodfill_free(t_game *game)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (game->floodfill[i])
+	{
+		free(game->floodfill[i]);
+		i++;
+	}
+	free(game->floodfill);
+}
+
+// Checks if the current position is a wall OR already visited.
+// Checks if the current position is the exit.
+// Recursively calls itself to check all four directions.
+static t_bool	fill(t_game *game, unsigned int x, unsigned int y)
+{
+	if (game->floodfill[x][y] == '1' || game->floodfill[x][y] == 'X')
+		return (false);
+	if (game->floodfill[x][y] == 'E')
+		return (true);
+	if (game->floodfill[x][y] == 'C')
+		col++;
+	game->floodfill[x][y] = 'X';
+	if (fill(game, x + 1, y) || fill(game, x, y + 1)
+		|| fill(game, x - 1, y) || fill(game, x, y - 1))
+		return (true);
+	return (false);
+}
+
+// Finds player's position to start the pathfinding.
+static t_bool	floodfill(t_game *game)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	while (i < game->height)
+	{
+		j = 0;
+		while (j < game->width)
+		{
+			if (game->floodfill[i][j] == 'P')
+			{
+				game->pos.x = i;
+				game->pos.y = j;
+				return (fill(game, game->pos.x, game->pos.y));
+			}
+			j++;
+		}
+		i++;
+	}
+	return (false);
+}
+
+// Duplicates the map to be used for Flood Fill pathfinding.
+static void	floodfill_alloc(t_game *game)
+{
+	unsigned int	i;
+	unsigned int	height;
+
+	i = 0;
+	height = game->height;
+	game->floodfill = (char **)malloc(sizeof(char *) * (height + 1));
+	if (!game->floodfill)
+		ft_puterr("Allocation Failed! [./src/map/path_map]", 1);
+	while (i < height)
+	{
+		game->floodfill[i] = ft_strdup((char *)game->map[i]);
+		if (!game->floodfill[i])
+			ft_puterr("Allocation Failed! [./src/map/path_map]", 1);
+		i++;
+	}
+	game->floodfill[i] = NULL;
+}
+
+t_bool	is_valid_path(t_game *game)
+{
+	t_bool	valid;
+
+	floodfill_alloc(game);
+	valid = floodfill(game);
+	floodfill_free(game);
+	return (valid);
+}
