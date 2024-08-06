36e457187cd1b73cd2d54dc4119faba021f5a4b0
flmarsou <marsouinflorent12@gmail.com>
2024-08-06 16:04:25 +0200

    Added More Sprites

--- src/display/init_sprites.c
+++ src/display/init_sprites.c
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 11:14:23 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/08/05 16:05:07 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/08/06 14:49:15 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,35 @@
 
 void	init_wall(t_game *game, int width, int height)
 {
-	game->mlx.xpm_wall = mlx_xpm_file_to_image(game->mlx.mlx,
-			SPRITE_WALL, &width, &height);
-	if (!game->mlx.xpm_wall)
-		ft_puterr("Couldn't load \"wall.xpm\" correctly!");
-}
+	// Upper Wall
+	game->mlx.xpm_upper_wall = mlx_xpm_file_to_image(game->mlx.mlx,
+			UPPER_WALL, &width, &height);
 
-void	init_ground(t_game *game, int width, int height)
-{
-	game->mlx.xpm_ground = mlx_xpm_file_to_image(game->mlx.mlx,
-			SPRITE_GROUND, &width, &height);
-	if (!game->mlx.xpm_ground)
-		ft_puterr("Couldn't load \"ground.xpm\" correctly!");
+	// Lower Wall
+	game->mlx.xpm_lower_wall = mlx_xpm_file_to_image(game->mlx.mlx,
+			LOWER_WALL, &width, &height);
+
+	// Upper Left Corner Wall
+	game->mlx.xpm_upper_lc_wall = mlx_xpm_file_to_image(game->mlx.mlx,
+			UPPER_LC_WALL, &width, &height);
+	
+	// Upper Right Corner Wall
+	game->mlx.xpm_upper_rc_wall = mlx_xpm_file_to_image(game->mlx.mlx,
+			UPPER_RC_WALL, &width, &height);
+
+	// Lower Left Corner Wall
+	game->mlx.xpm_lower_lc_wall = mlx_xpm_file_to_image(game->mlx.mlx,
+			LOWER_LC_WALL, &width, &height);
+	
+	// Lower Right Corner Wall
+	game->mlx.xpm_lower_rc_wall = mlx_xpm_file_to_image(game->mlx.mlx,
+			LOWER_RC_WALL, &width, &height);
+
+	// Horizontal Wall
+	game->mlx.xpm_left_wall = mlx_xpm_file_to_image(game->mlx.mlx,
+			LEFT_WALL, &width, &height);
+
+	// Vertical Wall
+	game->mlx.xpm_right_wall = mlx_xpm_file_to_image(game->mlx.mlx,
+			RIGHT_WALL, &width, &height);
 }
