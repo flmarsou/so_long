6bf9bffbc3ce097de905e25b546e56fbf64fe05b
flmarsou <marsouinflorent12@gmail.com>
2024-08-07 21:16:19 +0200

    Update init_sprites.c

--- src/display/init_sprites.c
+++ src/display/init_sprites.c
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 11:14:23 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/08/06 14:49:15 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/08/07 21:04:51 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,25 @@
 
 void	init_wall(t_game *game, int width, int height)
 {
-	// Upper Wall
-	game->mlx.xpm_upper_wall = mlx_xpm_file_to_image(game->mlx.mlx,
-			UPPER_WALL, &width, &height);
-
-	// Lower Wall
-	game->mlx.xpm_lower_wall = mlx_xpm_file_to_image(game->mlx.mlx,
-			LOWER_WALL, &width, &height);
-
-	// Upper Left Corner Wall
-	game->mlx.xpm_upper_lc_wall = mlx_xpm_file_to_image(game->mlx.mlx,
-			UPPER_LC_WALL, &width, &height);
-	
-	// Upper Right Corner Wall
-	game->mlx.xpm_upper_rc_wall = mlx_xpm_file_to_image(game->mlx.mlx,
-			UPPER_RC_WALL, &width, &height);
-
-	// Lower Left Corner Wall
-	game->mlx.xpm_lower_lc_wall = mlx_xpm_file_to_image(game->mlx.mlx,
-			LOWER_LC_WALL, &width, &height);
-	
-	// Lower Right Corner Wall
-	game->mlx.xpm_lower_rc_wall = mlx_xpm_file_to_image(game->mlx.mlx,
-			LOWER_RC_WALL, &width, &height);
-
-	// Horizontal Wall
-	game->mlx.xpm_left_wall = mlx_xpm_file_to_image(game->mlx.mlx,
-			LEFT_WALL, &width, &height);
-
-	// Vertical Wall
-	game->mlx.xpm_right_wall = mlx_xpm_file_to_image(game->mlx.mlx,
-			RIGHT_WALL, &width, &height);
+	game->mlx.wall.up = mlx_xpm_file_to_image(game->mlx.mlx,
+			WALL_UP, &width, &height);
+	game->mlx.wall.down = mlx_xpm_file_to_image(game->mlx.mlx,
+			WALL_DOWN, &width, &height);
+	game->mlx.wall.left = mlx_xpm_file_to_image(game->mlx.mlx,
+			WALL_LEFT, &width, &height);
+	game->mlx.wall.right = mlx_xpm_file_to_image(game->mlx.mlx,
+			WALL_RIGHT, &width, &height);
+	game->mlx.wall.up_left_corner = mlx_xpm_file_to_image(game->mlx.mlx,
+			WALL_UP_LEFT_CORNER, &width, &height);
+	game->mlx.wall.up_right_corner = mlx_xpm_file_to_image(game->mlx.mlx,
+			WALL_UP_RIGHT_CORNER, &width, &height);
+	game->mlx.wall.down_left_corner = mlx_xpm_file_to_image(game->mlx.mlx,
+			WALL_DOWN_LEFT_CORNER, &width, &height);
+	game->mlx.wall.down_right_corner = mlx_xpm_file_to_image(game->mlx.mlx,
+			WALL_DOWN_RIGHT_CORNER, &width, &height);
+	if (!(game->mlx.wall.up && game->mlx.wall.down && game->mlx.wall.left
+			&& game->mlx.wall.right && game->mlx.wall.up_left_corner
+			&& game->mlx.wall.up_right_corner && game->mlx.wall.down_left_corner
+			&& game->mlx.wall.down_right_corner))
+		ft_puterr("Couldn't initialize wall textures!");
 }
