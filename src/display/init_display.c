01a643c3fcb45a3d88d7f24a5bb1b7963037c6bf
flmarsou <marsouinflorent12@gmail.com>
2024-08-07 21:16:23 +0200

    Update init_display.c

--- src/display/init_display.c
+++ src/display/init_display.c
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 10:55:37 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/08/06 14:14:59 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/08/07 21:05:51 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,6 @@
 
 void	init_display(t_game *game)
 {
-	int	width;
-	int	height;
-
-	width = TILES;
-	height = TILES;
 	game->mlx.mlx = mlx_init();
 	if (!game->mlx.mlx)
 		ft_puterr("Failed to initialize MLX!");
@@ -26,9 +21,5 @@ void	init_display(t_game *game)
 			game->height * TILES, "So Long by flmarsou");
 	if (!game->mlx.win)
 		ft_puterr("Failed to create window!");
-
 	init_wall(game, TILES, TILES);
-
-	draw_walls(game);
 }
-
