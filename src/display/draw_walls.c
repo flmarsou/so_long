ea482fe46b20a3da9b13b8430088e44beeb3b918
flmarsou <marsouinflorent12@gmail.com>
2024-08-08 15:54:08 +0200

    Finished Wall Algo

--- src/display/draw_walls.c
+++ src/display/draw_walls.c
@@ -6,92 +6,161 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 09:50:42 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/08/06 17:06:53 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/08/08 15:33:28 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/so_long.h"
 
-// static void	display_wall(t_game *game, int wall, unsigned int x, unsigned int y)
-// {
-// 	if (wall == 1)
-// 		mlx_put_image_to_window(game->mlx.mlx, game->mlx.win, game->mlx.xpm_upper_lc_wall, x, y);
-// 	else if (wall == 2)
-// 		mlx_put_image_to_window(game->mlx.mlx, game->mlx.win, game->mlx.xpm_upper_wall, x, y);
-// 	else if (wall == 3)
-// 		mlx_put_image_to_window(game->mlx.mlx, game->mlx.win, game->mlx.xpm_upper_rc_wall, x, y);
-// 	else if (wall == 4)
-// 		mlx_put_image_to_window(game->mlx.mlx, game->mlx.win, game->mlx.xpm_left_wall, x, y);
-// 	else if (wall == 5)
-// 		mlx_put_image_to_window(game->mlx.mlx, game->mlx.win, game->mlx.xpm_lower_lc_wall, x, y);
-// 	else if (wall == 6)
-// 		mlx_put_image_to_window(game->mlx.mlx, game->mlx.win, game->mlx.xpm_lower_wall, x, y);
-// 	else if (wall == 7)
-// 		mlx_put_image_to_window(game->mlx.mlx, game->mlx.win, game->mlx.xpm_lower_rc_wall, x, y);
-// 	else if (wall == 8)
-// 		mlx_put_image_to_window(game->mlx.mlx, game->mlx.win, game->mlx.xpm_right_wall, x, y);
-// }
+static void	draw_wall2(t_game *game, int wall, unsigned int x, unsigned int y)
+{
+	if (wall == 6)
+		mlx_put_image_to_window(game->mlx.mlx, game->mlx.win,
+			game->mlx.wall.up_right_corner, x, y);
+	else if (wall == 7)
+		mlx_put_image_to_window(game->mlx.mlx, game->mlx.win,
+			game->mlx.wall.down_left_corner, x, y);
+	else if (wall == 8)
+		mlx_put_image_to_window(game->mlx.mlx, game->mlx.win,
+			game->mlx.wall.down_right_corner, x, y);
+	else if (wall == 9)
+		mlx_put_image_to_window(game->mlx.mlx, game->mlx.win,
+			game->mlx.wall.vertical, x, y);
+	else if (wall == 10)
+		mlx_put_image_to_window(game->mlx.mlx, game->mlx.win,
+			game->mlx.wall.background, x, y);
+	else if (wall == 11)
+		mlx_put_image_to_window(game->mlx.mlx, game->mlx.win,
+			game->mlx.wall.background, x, y);
+}
 
-// // 1: Upper Left Corner Wall
-// // 2: Upper Wall
-// // 3: Upper Right Corner Wall
-// // 4: Left Wall
-// // 5: Lower Left Corner Wall
-// // 6: Lower Wall
-// // 7: Lower Right Corner
-// // 8: Right Wall
-// static int	which_wall(t_game *game, unsigned int x, unsigned int y)
-// {
-// 	t_bool	top = (y == 0);
-// 	t_bool	bottom = (y == game->height - 1);
-// 	t_bool	left = (x == 0);
-// 	t_bool	right = (x == game->width - 1);
-// 	t_bool	top_wall = top || game->map[y - 1][x] == '1';
-// 	t_bool	bottom_wall = bottom || game->map[y + 1][x] == '1';
-// 	t_bool	left_wall = left || game->map[y][x - 1] == '1';
-// 	t_bool	right_wall = right || game->map[y][x + 1] == '1';
+static void	draw_wall1(t_game *game, int wall, unsigned int x, unsigned int y)
+{
+	if (wall == 0)
+		mlx_put_image_to_window(game->mlx.mlx, game->mlx.win,
+			game->mlx.wall.background, x, y);
+	else if (wall == 1)
+		mlx_put_image_to_window(game->mlx.mlx, game->mlx.win,
+			game->mlx.wall.up, x, y);
+	else if (wall == 2)
+		mlx_put_image_to_window(game->mlx.mlx, game->mlx.win,
+			game->mlx.wall.down, x, y);
+	else if (wall == 3)
+		mlx_put_image_to_window(game->mlx.mlx, game->mlx.win,
+			game->mlx.wall.left, x, y);
+	else if (wall == 4)
+		mlx_put_image_to_window(game->mlx.mlx, game->mlx.win,
+			game->mlx.wall.right, x, y);
+	else if (wall == 5)
+		mlx_put_image_to_window(game->mlx.mlx, game->mlx.win,
+			game->mlx.wall.up_left_corner, x, y);
+	else
+		draw_wall2(game, wall, x, y);
+}
 
-//     if (!top_wall && !left_wall)
-// 		return (1);
-//     if (!top_wall && right_wall && left_wall)
-// 		return (2);
-//     if (!top_wall && !right_wall)
-// 		return (3);
-//     if (top_wall && bottom_wall && !left_wall && right_wall)
-// 		return (4);
-//     if (!bottom_wall && !left_wall)
-// 		return (5);
-//     if (!bottom_wall && right_wall && left_wall)
-// 		return (6);
-//     if (!bottom_wall && !right_wall)
-// 		return (7);
-//     if (top_wall && bottom_wall && !right_wall && left_wall)
-// 		return (8);
-//     return (0);
-// }
+/* ************************************************************************** */
+/* Top Wall:	True if there is a wall above the current tile.               */
+/* Bottom Wall:	True if there is a wall below the current tile.               */
+/* Left Wall:	True if there is a wall to the left of the current tile.      */
+/* Right Wall:	True if there is a wall to the right of the current tile.     */
+/*                                                                            */
+/* | 1:     1        | 2:     0        | 3:     0        | 4:     0        |  */
+/* |       1X1       |       1X1       |       0X1       |       1X0       |  */
+/* |        0    = 1 |        0    = 1 |        0    = 1 |        0    = 1 |  */
+/*                                                                            */
+/* | 5:     0        | 6:     1        | 7:     1        | 8:     0        |  */
+/* |       1X1       |       1X0       |       0X1       |       1X0       |  */
+/* |        1    = 2 |        1    = 3 |        1    = 4 |        1    = 5 |  */
+/*                                                                            */
+/* | 9:     0        | 10:    1        | 11:    1        | 12:    1        |  */
+/* |       0X1       |       0X1       |       1X0       |       0X0       |  */
+/* |        1    = 6 |        0    = 7 |        0    = 8 |        1    = 9 |  */
+/*                                                                            */
+/* | 13:    0        | 14:    1        | 15:    1        |                 |  */
+/* |       0X0       |       0X0       |       1X1       |                 |  */
+/* |        1   = 10 |        0   = 11 |        1    = 0 |                 |  */
+/* ************************************************************************** */
+static int	which_wall1(t_game *game, unsigned int x, unsigned int y)
+{
+	t_bool	top_wall;
+	t_bool	bottom_wall;
+	t_bool	left_wall;
+	t_bool	right_wall;
 
-// void	draw_walls(t_game *game)
-// {
-// 	static unsigned int	y;
-// 	static unsigned int	x;
-// 	static unsigned int	y_offset;
-// 	static unsigned int	x_offset;
+	top_wall = (y == 0) || game->map[y - 1][x] == '1';
+	bottom_wall = (y == game->height - 1) || game->map[y + 1][x] == '1';
+	left_wall = (x == 0) || game->map[y][x - 1] == '1';
+	right_wall = (x == game->width - 1) || game->map[y][x + 1] == '1';
+	if (top_wall && !bottom_wall && left_wall && right_wall)
+		return (1);
+	if (!top_wall && !bottom_wall && left_wall && right_wall)
+		return (1);
+	if (!top_wall && !bottom_wall && !left_wall && right_wall)
+		return (1);
+	if (!top_wall && !bottom_wall && left_wall && !right_wall)
+		return (1);
+	if (!top_wall && bottom_wall && left_wall && right_wall)
+		return (2);
+	if (top_wall && bottom_wall && left_wall && !right_wall)
+		return (3);
+	if (top_wall && bottom_wall && !left_wall && right_wall)
+		return (4);
+	return (-1);
+}
 
-// 	y = 0;
-// 	y_offset = 0;
-// 	while (y < game->height)
-// 	{
-// 		x = 0;
-// 		x_offset = 0;
-// 		while (x < game->width)
-// 		{
-// 			if (game->map[y][x] == '1')
-// 				display_wall(game, which_wall(game, x, y), x_offset, y_offset);
-// 			x++;
-// 			x_offset += TILES;
-// 		}
-// 		x = 0;
-// 		y_offset += TILES;
-// 		y++;
-// 	}
-// }
+static int	which_wall2(t_game *game, unsigned int x, unsigned int y)
+{
+	t_bool	top_wall;
+	t_bool	bottom_wall;
+	t_bool	left_wall;
+	t_bool	right_wall;
+
+	top_wall = (y == 0) || game->map[y - 1][x] == '1';
+	bottom_wall = (y == game->height - 1) || game->map[y + 1][x] == '1';
+	left_wall = (x == 0) || game->map[y][x - 1] == '1';
+	right_wall = (x == game->width - 1) || game->map[y][x + 1] == '1';
+	if (!top_wall && bottom_wall && left_wall && !right_wall)
+		return (5);
+	if (!top_wall && bottom_wall && !left_wall && right_wall)
+		return (6);
+	if (top_wall && !bottom_wall && !left_wall && right_wall)
+		return (7);
+	if (top_wall && !bottom_wall && left_wall && !right_wall)
+		return (8);
+	if (top_wall && bottom_wall && !left_wall && !right_wall)
+		return (9);
+	if (!top_wall && bottom_wall && !left_wall && !right_wall)
+		return (10);
+	if (top_wall && !bottom_wall && !left_wall && !right_wall)
+		return (11);
+	return (0);
+}
+
+void	draw_walls(t_game *game)
+{
+	unsigned int	y;
+	unsigned int	x;
+	unsigned int	y_offset;
+	unsigned int	x_offset;
+	int				wall;
+
+	y = -1;
+	y_offset = 0;
+	while (++y < game->height)
+	{
+		x = -1;
+		x_offset = 0;
+		while (++x < game->width)	
+		{
+			if (game->map[y][x] == '1')
+			{
+				wall = which_wall1(game, x, y);
+				if (wall == -1)
+					wall = which_wall2(game, x, y);
+				draw_wall1(game, wall, x_offset, y_offset);
+			}
+			x_offset += TILES;
+		}
+		y_offset += TILES;
+	}
+}
