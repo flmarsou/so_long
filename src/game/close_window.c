a2e93bc5b037c6dc4bb6504826cafbdef8c1125c
flmarsou <marsouinflorent12@gmail.com>
2024-08-22 15:39:29 +0200

    Update close_window.c

--- src/game/close_window.c
+++ src/game/close_window.c
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 14:22:05 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/08/20 15:07:24 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/08/22 14:47:50 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,13 @@
 int	close_window(t_game *game)
 {
 	if (game->mlx.wall.background)
-        mlx_destroy_image(game->mlx.mlx, game->mlx.wall.background);
+		mlx_destroy_image(game->mlx.mlx, game->mlx.wall.background);
 	if (game->mlx.floor.floor1)
-        mlx_destroy_image(game->mlx.mlx, game->mlx.floor.floor1);
+		mlx_destroy_image(game->mlx.mlx, game->mlx.floor.floor1);
+	if (game->mlx.player.player_up)
+		mlx_destroy_image(game->mlx.mlx, game->mlx.player.player_up);
 	if (game->mlx.win)
-        mlx_destroy_window(game->mlx.mlx, game->mlx.win);
+		mlx_destroy_window(game->mlx.mlx, game->mlx.win);
 	free_map(game);
 	free(game);
 	ft_putstr("\e[1;36m[!] - Info: \e[1;97mGame closed!\n\e[0m");
