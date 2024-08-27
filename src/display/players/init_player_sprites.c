a5c8e8ae950e1172a2b486b2b86be01eea83b27a
flmarsou <marsouinflorent12@gmail.com>
2024-08-27 12:50:08 +0200

    Added Player Sprites

--- src/display/players/init_player_sprites.c
+++ src/display/players/init_player_sprites.c
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 14:44:42 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/08/22 14:50:32 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/08/27 12:47:30 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,10 @@ void	init_player_sprites(t_game *game, int width, int height)
 {
 	game->mlx.player.player_up = mlx_xpm_file_to_image(game->mlx.mlx,
 			PLAYER_UP, &width, &height);
+	game->mlx.player.player_down = mlx_xpm_file_to_image(game->mlx.mlx,
+			PLAYER_DOWN, &width, &height);
+	game->mlx.player.player_left = mlx_xpm_file_to_image(game->mlx.mlx,
+			PLAYER_LEFT, &width, &height);
+	game->mlx.player.player_right = mlx_xpm_file_to_image(game->mlx.mlx,
+			PLAYER_RIGHT, &width, &height);
 }
