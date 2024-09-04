324fa522d2e5d032aaae890d7e5820abbbd2a2b9
flmarsou <marsouinflorent12@gmail.com>
2024-09-04 13:24:24 +0200

    Better and Shorter functions

--- src/game/player_move.c
+++ src/game/player_move.c
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 10:00:04 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/09/03 15:06:53 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/09/04 13:21:33 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,20 @@
 
 static void	redraw(t_game *game, unsigned int x, unsigned int y, int face)
 {
-	unsigned int	player_x;
-	unsigned int	player_y;
-
-	player_x = game->pos.x;
-	player_y = game->pos.y;
-	draw_floor(game, random_floor(game), x * TILES, y * TILES);
+	draw_floor(game, random_floor(game),
+		game->pos.x * TILES, game->pos.y * TILES);
 	if (face == W)
 		mlx_put_image_to_window(game->mlx.mlx, game->mlx.win,
-			game->mlx.player.player_up, player_x * TILES, player_y * TILES);
+			game->mlx.player.player_up, x * TILES, y * TILES);
 	else if (face == A)
 		mlx_put_image_to_window(game->mlx.mlx, game->mlx.win,
-			game->mlx.player.player_left, player_x * TILES, player_y * TILES);
+			game->mlx.player.player_left, x * TILES, y * TILES);
 	else if (face == S)
 		mlx_put_image_to_window(game->mlx.mlx, game->mlx.win,
-			game->mlx.player.player_down, player_x * TILES, player_y * TILES);
+			game->mlx.player.player_down, x * TILES, y * TILES);
 	else
 		mlx_put_image_to_window(game->mlx.mlx, game->mlx.win,
-			game->mlx.player.player_right, player_x * TILES, player_y * TILES);
+			game->mlx.player.player_right, x * TILES, y * TILES);
 }
 
 static t_bool	is_dead(t_game *game)
@@ -54,21 +50,16 @@ static t_bool	is_dead(t_game *game)
 
 void	player_move(t_game *game, unsigned int x, unsigned int y, int face)
 {
-	unsigned int	player_x;
-	unsigned int	player_y;
-
-	player_x = game->pos.x;
-	player_y = game->pos.y;
 	if (game->map[y][x] == '0' || game->map[y][x] == 'C')
 	{
 		if (game->map[y][x] == 'C')
 			game->count.collectible--;
-		game->map[player_y][player_x] = '0';
+		game->map[game->pos.y][game->pos.x] = '0';
 		game->map[y][x] = 'P';
+		game->count.moves++;
+		redraw(game, x, y, face);
 		game->pos.x = x;
 		game->pos.y = y;
-		game->count.moves++;
-		redraw(game, player_x, player_y, face);
 	}
 	if (game->map[y][x] == 'E' && game->count.collectible == 0)
 		close_window(game);
