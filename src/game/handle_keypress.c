652abbd8c4e68e295478f1682290473410a40aa2
flmarsou <marsouinflorent12@gmail.com>
2024-09-05 13:56:31 +0200

    Fixed player death bug

--- src/game/handle_keypress.c
+++ src/game/handle_keypress.c
@@ -6,33 +6,62 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 14:58:18 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/09/04 11:48:30 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/09/05 12:38:30 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/so_long.h"
 
-int	handle_keypress(int key, t_game *game)
+static t_bool	is_dead(t_game *game)
 {
-	unsigned int	temp_x;
-	unsigned int	temp_y;
+	unsigned int	y;
+	unsigned int	x;
 
-	temp_x = game->pos.x;
-	temp_y = game->pos.y;
-	if (key == ESC)
-		close_window(game);
-	if (key == W || key == ARROW_UP)
-		player_move(game, temp_x, --temp_y, W);
-	else if (key == A || key == ARROW_LEFT)
-		player_move(game, --temp_x, temp_y, A);
-	else if (key == S || key == ARROW_DOWN)
-		player_move(game, temp_x, ++temp_y, S);
-	else if (key == D || key == ARROW_RIGHT)
-		player_move(game, ++temp_x, temp_y, D);
-	else
-		return (0);
+	y = -1;
+	while (++y < game->height)
+	{
+		x = -1;
+		while (++x < game->width)
+		{
+			if (game->map[y][x] == 'P')
+				return (false);
+		}
+	}
+	return (true);
+}
+
+static void	others(t_game *game)
+{
 	enemy_move(game);
 	display_exit(game);
 	handle_text(game);
+	print_map(game);
+	if (is_dead(game))
+	{
+		ft_putstr("\e[1;36m[!] - Info: \e[1;97mYou died...\n\e[0m");
+		close_window(game);
+	}
+}
+
+int	handle_keypress(int key, t_game *game)
+{
+	unsigned int	x;
+	unsigned int	y;
+
+	x = game->pos.x;
+	y = game->pos.y;
+	if (key == ESC)
+		close_window(game);
+	if (key == W || key == ARROW_UP)
+		player_move(game, x, --y, W);
+	else if (key == A || key == ARROW_LEFT)
+		player_move(game, --x, y, A);
+	else if (key == S || key == ARROW_DOWN)
+		player_move(game, x, ++y, S);
+	else if (key == D || key == ARROW_RIGHT)
+		player_move(game, ++x, y, D);
+	else
+		return (0);
+	others(game);
 	return (0);
 }
--- src/game/handle_text.c
+++ src/game/handle_text.c
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 09:00:25 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/08/26 11:55:34 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/09/05 12:45:21 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ static void	count_text(t_game *game)
 		- 28, 13, 0x555555, count);
 	mlx_string_put(game->mlx.mlx, game->mlx.win, (game->width * TILES)
 		- 30, 11, 0xFFFFFF, count);
-	ft_printf("Movement Count: %s\n", count);
+	ft_printf("\e[1;36m[!] - Info: \e[1;97mMovement Count:\e[0m %s\n", count);
 	free(count);
 }
 
@@ -46,13 +46,11 @@ static void	info_text(t_game *game)
 	mlx_string_put(game->mlx.mlx, game->mlx.win, 5, 48, 0x555555, "LEFT  - A");
 	mlx_string_put(game->mlx.mlx, game->mlx.win, 5, 63, 0x555555, "DOWN  - S");
 	mlx_string_put(game->mlx.mlx, game->mlx.win, 5, 78, 0x555555, "RIGHT - D");
-	mlx_string_put(game->mlx.mlx, game->mlx.win, 5, 98, 0x555555, "RESET - R");
 	mlx_string_put(game->mlx.mlx, game->mlx.win, 3, 11, 0xFFFFFF, "Keys:");
 	mlx_string_put(game->mlx.mlx, game->mlx.win, 3, 31, 0xFFFFFF, "UP    - W");
 	mlx_string_put(game->mlx.mlx, game->mlx.win, 3, 46, 0xFFFFFF, "LEFT  - A");
 	mlx_string_put(game->mlx.mlx, game->mlx.win, 3, 61, 0xFFFFFF, "DOWN  - S");
 	mlx_string_put(game->mlx.mlx, game->mlx.win, 3, 76, 0xFFFFFF, "RIGHT - D");
-	mlx_string_put(game->mlx.mlx, game->mlx.win, 3, 96, 0xFFFFFF, "RESET - R");
 }
 
 static void	draw_box_count(t_game *game)
@@ -60,20 +58,24 @@ static void	draw_box_count(t_game *game)
 	unsigned int	i;
 
 	i = -1;
+	mlx_pixel_put(game->mlx.mlx, game->mlx.win,
+		(game->width * TILES) - 70 + i, 15, 0x555555);
 	while (++i < 100)
 	{
-		mlx_pixel_put(game->mlx.mlx, game->mlx.win, (game->width * TILES)
-			- 72 + i, 0, 0xFFFFFF);
-		mlx_pixel_put(game->mlx.mlx, game->mlx.win, (game->width * TILES)
-			- 72 + i, 14, 0xFFFFFF);
+		mlx_pixel_put(game->mlx.mlx, game->mlx.win,
+			(game->width * TILES) - 72 + i, 0, 0xFFFFFF);
+		mlx_pixel_put(game->mlx.mlx, game->mlx.win,
+			(game->width * TILES) - 71 + i, 16, 0x555555);
+		mlx_pixel_put(game->mlx.mlx, game->mlx.win,
+			(game->width * TILES) - 72 + i, 14, 0xFFFFFF);
 	}
 	i = -1;
 	while (++i < 14)
 	{
-		mlx_pixel_put(game->mlx.mlx, game->mlx.win, (game->width * TILES)
-			- 72, i, 0xFFFFFF);
-		mlx_pixel_put(game->mlx.mlx, game->mlx.win, (game->width * TILES)
-			- 1, i, 0xFFFFFF);
+		mlx_pixel_put(game->mlx.mlx, game->mlx.win,
+			(game->width * TILES) - 72, i, 0xFFFFFF);
+		mlx_pixel_put(game->mlx.mlx, game->mlx.win,
+			(game->width * TILES) - 1, i, 0xFFFFFF);
 	}
 }
 
@@ -82,16 +84,16 @@ static void	draw_box_info(t_game *game)
 	unsigned int	i;
 
 	i = -1;
-	mlx_pixel_put(game->mlx.mlx, game->mlx.win, 2, 100, 0x555555);
+	mlx_pixel_put(game->mlx.mlx, game->mlx.win, 2, 80, 0x555555);
 	mlx_pixel_put(game->mlx.mlx, game->mlx.win, 60, 2, 0x555555);
 	while (++i < 60)
 	{
-		mlx_pixel_put(game->mlx.mlx, game->mlx.win, i + 2, 101, 0x555555);
+		mlx_pixel_put(game->mlx.mlx, game->mlx.win, i + 2, 81, 0x555555);
 		mlx_pixel_put(game->mlx.mlx, game->mlx.win, i, 0, 0xFFFFFF);
-		mlx_pixel_put(game->mlx.mlx, game->mlx.win, i, 99, 0xFFFFFF);
+		mlx_pixel_put(game->mlx.mlx, game->mlx.win, i, 79, 0xFFFFFF);
 	}
 	i = -1;
-	while (++i < 99)
+	while (++i < 79)
 	{
 		mlx_pixel_put(game->mlx.mlx, game->mlx.win, 61, i + 2, 0x555555);
 		mlx_pixel_put(game->mlx.mlx, game->mlx.win, 0, i, 0xFFFFFF);
@@ -105,4 +107,6 @@ void	handle_text(t_game *game)
 	draw_box_count(game);
 	info_text(game);
 	count_text(game);
+	ft_printf("\e[1;36m[!] - Info: \e[1;97mCollectible Left:\e[0m %u\n",
+		game->count.collectible);
 }
